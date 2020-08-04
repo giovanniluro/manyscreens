d38ad9ea964b3c5d7ffb209efad181bccf5f3cff
Giovanni Luro <giovanni.luro@outlook.com>
2020-08-04 00:19:20 -0300

    Updates

---
+++ precache-manifest.c1c9a83dab4dd9e5da849cb4d958a3cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec97cbde74ba763d2885f97580da52a2",
+    "url": "/manyscreens/index.html"
+  },
+  {
+    "revision": "1b4973825ec1ad247451",
+    "url": "/manyscreens/static/js/2.dd04c280.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/manyscreens/static/js/2.dd04c280.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b77832f4eb14f8c6354b",
+    "url": "/manyscreens/static/js/main.980b621d.chunk.js"
+  },
+  {
+    "revision": "806483e789a6723e73bb",
+    "url": "/manyscreens/static/js/runtime-main.3939a767.js"
+  }
+]);
